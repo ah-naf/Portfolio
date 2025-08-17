593ab59beac67a478ef9ebd9f92462c98d1afd6f
ah-naf <sheikhahnafshifat@gmail.com>
2025-08-17 11:59:26 +0600

    move skill, projects and blog data to its own file

---
+++ src/data/blogs.js
@@ -0,0 +1,43 @@
+export const mediumPosts = [
+  {
+    title: "Rate Limiting 101: Implementing in Go",
+    link: "https://medium.com/@sheikhahnafshifat/rate-limiting-101-implementing-in-go-c434675f1fbe",
+  },
+  {
+    title: "Merkle Tree 201: Building a Go CLI to Verify Directory",
+    link: "https://medium.com/@sheikhahnafshifat/merkle-proofs-201-building-a-go-cli-to-verify-directory-7f02da1e15fd",
+  },
+  {
+    title: "Merkle Tree 101: Create a CLI Tool That Outputs JSON using Golang",
+    link: "https://medium.com/@sheikhahnafshifat/merkle-tree-101-create-a-cli-tool-that-outputs-json-using-golang-83ce3b87a65e",
+  },
+  {
+    title: "Getting Started with Prometheus Monitoring in Go",
+    link: "https://medium.com/@sheikhahnafshifat/getting-started-with-prometheus-monitoring-in-go-3f8fd3d4e19f",
+  },
+  {
+    title: "Forwarding Layer 4 (HTTP) Traffic in Mininet Using OpenFlow",
+    link: "https://medium.com/@sheikhahnafshifat/forwarding-layer-4-http-traffic-in-mininet-using-openflow-b7a374f2152c",
+  },
+  {
+    title: "Layer 3 Traffic Forwarding Setup in Mininet with OpenFlow",
+    link: "https://medium.com/@sheikhahnafshifat/layer-3-traffic-forwarding-setup-in-mininet-with-openflow-3ffa6b1eca44",
+  },
+  {
+    title:
+      "MAC Address‑Based Layer 2 Traffic Forwarding in Mininet Using OpenFlow",
+    link: "https://medium.com/@sheikhahnafshifat/mac-address-based-layer-2-traffic-forwarding-in-mininet-using-openflow-01b3c03050a2",
+  },
+  {
+    title: "Simulating a Simple Network with Mininet",
+    link: "https://medium.com/@sheikhahnafshifat/simulating-a-simple-network-with-mininet-f61fe9cc30f9",
+  },
+  {
+    title: "Software Defined Networking (SDN)",
+    link: "https://medium.com/@sheikhahnafshifat/software-defined-networking-sdn-0ef37e1d8bc8",
+  },
+  {
+    title: "Networking 101",
+    link: "https://medium.com/@sheikhahnafshifat/networking-101-de0112106d13",
+  },
+];
---
+++ src/data/skills.js
@@ -0,0 +1,12 @@
+export const skills = {
+  "Programming Languages": ["C", "C++", "JavaScript", "SQL", "Golang"],
+  "Web Frameworks": ["React", "Express (Node.js)", "Gin (Go)"],
+  "API Development": [
+    "REST API",
+    "Postman",
+    "Authentication/Authorization",
+    "JWT",
+  ],
+  "DevOps & Tools": ["Docker", "GitHub CI/CD", "Git", "VS Code", "Linux"],
+  "Familiar With": ["MongoDB"],
+};
